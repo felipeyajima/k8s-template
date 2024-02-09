d6f71d1d0a1962fb0fd82a430eef3101544315d4
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-02-09 20:51:11 -0300

    Update nginx-dp.tf

--- nginx-dp.tf
+++ nginx-dp.tf
@@ -1,3 +1,15 @@
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
+
 /*
 resource "kubernetes_deployment" "nginx" {
   metadata {
