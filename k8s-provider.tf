b83ee286ace9db306a1889a5a9007c97f608d7ac
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-02-09 20:50:10 -0300

    Update k8s-provider.tf

--- k8s-provider.tf
+++ k8s-provider.tf
@@ -14,3 +14,14 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 
 }
+
+
+provider "helm" {
+  kubernetes {
+  host = var.host
+
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+  }
+}
