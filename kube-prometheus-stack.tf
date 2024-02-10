a27bd4ac0588b68b04cb10bcee63b638bd9763bb
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-02-09 22:28:36 -0300

    Update and rename nginx-dp.tf to kube-prometheus-stack.tf

--- kube-prometheus-stack.tf
+++ kube-prometheus-stack.tf
@@ -7,20 +7,6 @@ resource "helm_release" "kube-prometheus" {
   create_namespace = true
 }
 
-/*
-resource "helm_release" "prometheus" {
-  chart      = "prometheus"
-  name       = "prometheus"
-  namespace  = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  version    = "25.9.0"
-  create_namespace = true
-  timeout = "600"
-
-  set {
-    name  = "podSecurityPolicy.enabled"
-    value = true
-  }
 
   set {
     name  = "server.persistentVolume.enabled"
