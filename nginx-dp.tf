b0e301f50190f0d790318d8355abaed05b0d9a37
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-02-09 22:20:50 -0300

    Update nginx-dp.tf

--- nginx-dp.tf
+++ nginx-dp.tf
@@ -1,3 +1,11 @@
+resource "helm_release" "kube-prometheus" {
+  name       = "kube-prometheus-stack"
+  namespace  = "monitoring"
+  version    = "56.6.2"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  create_namespace = true
+}
 
 /*
 resource "helm_release" "prometheus" {
