df50ffac6cb5b090db3b306b55cf2c3cdf6f417e
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2025-05-12 21:42:48 -0300

    Update otel-collector.tf

--- otel-collector.tf
+++ otel-collector.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "otel-collector" {
   name       = "otel-collector"
   namespace  = "monitoring"
-  version    = "0.122.6"
+  version    = "opentelemetry-collector-0.122.6"
   repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart      = "opentelemetry-collector"
   create_namespace = true
