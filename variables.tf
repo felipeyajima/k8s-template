7c293ab1df93b58d0043c81c3f7eee914dfa5a1a
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2025-05-15 20:51:06 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,36 @@
 variable "host" {
+  description = "Host do K8s"
+  type        = string
+  sensitive   = true
 }
 
 variable "client_certificate" {
+  description = "Certificado do K8s"
+  type        = string
+  sensitive   = true
 }
 
 variable "client_key" {
+  description = "Client Key do K8s"
+  type        = string
+  sensitive   = true
 }
 
 variable "cluster_ca_certificate" {
+  description = "CA do K8s"
+  type        = string
+  sensitive   = true
 }
 
 variable "grafana_cloud_instance_id" {
+  description = "User Grafana Cloud"
+  type        = string
+  sensitive   = true
 }
 
 variable "grafana_cloud_api_key" {
+  description = "Pass do Grafana Cloud"
+  type        = string
+  sensitive   = true
 }
 
