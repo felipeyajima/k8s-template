f0065c67b280cb07443289b09f37df263c068d9d
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2025-05-15 21:01:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "open-telemetry" {
-  source = "./open-telemetry"
+  source = "./open-telemetry-collector"
 
   grafana_cloud_instance_id = var.grafana_cloud_instance_id
   grafana_cloud_api_key     = var.grafana_cloud_api_key
