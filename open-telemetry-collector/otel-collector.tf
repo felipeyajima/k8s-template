d71d4eb988273adb1778eeba9f9b61b0d7d4cb11
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2025-05-21 21:12:31 -0300

    Update otel-collector.tf

--- open-telemetry-collector/otel-collector.tf
+++ open-telemetry-collector/otel-collector.tf
@@ -26,11 +26,11 @@ resource "helm_release" "otel-collector" {
   }
   set {
     name  = "config.extensions.basicauth/grafana_cloud.client_auth.username"
-    value = var.grafana_cloud_instance_id
+    value = base64decode(var.grafana_cloud_instance_id)
   }
   set {
     name  = "config.extensions.basicauth/grafana_cloud.client_auth.password"
-    value = var.grafana_cloud_api_key
+    value = base64decode(var.grafana_cloud_api_key)
   }
 
 
